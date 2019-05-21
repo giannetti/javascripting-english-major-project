6cfa156f12bca141b1ddf485ba76b4f94ad45e99
Francesca Giannetti <francesca.giannetti@gmail.com>
2019-05-20 22:49:09 -0400

    added even/odd program

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-alert("scripts.js has loaded!");
+let userInput;
+userInput = prompt("Guess a number, you cheeky monkey!", "Type your answer here.");
+ 	for (let i = 1; i <= userInput; i = i + 1 ) {
+// 		$("#response").append("<br />" + i);
+		if (i % 2 === 0) {
+ 			$("#response").append(i + "<span> is even.</span><br />");
+ 		} else {
+ 			$("#response").append(i + "<span> FUCK! </span><br />");
+ 		}
+ 	}
